fc3c73bd50d181148754b38808b5e1d034c898cf
stefania.gutu <stefania.gutu@essensys.ro>
2023-04-17 23:25:42 +0300

    Add bottom navigation

--- app/src/main/java/com/example/shoppinglist1/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist1/MainActivity.kt
@@ -4,18 +4,27 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.shoppinglist1.databinding.ActivityMainBinding
+import com.example.shoppinglist1.databinding.FragmentAddListBinding
 
 const val USER_EMAIL = "user_email"
 const val USER_FULLNAME = "user_fullname"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var userEmail: String
+    lateinit var userFullname: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val KEY_WELCOME = "welcome"
 
-        val userEmail = intent.getStringExtra("userEmail")
-        val userFullname = intent.getStringExtra("userFullname")
+        userEmail = intent.getStringExtra("userEmail")!!
+        userFullname = intent.getStringExtra("userFullname")!!
 
         val sharedPreferences: SharedPreferences =
             getSharedPreferences("SmartShoppingPreferences", Context.MODE_PRIVATE)
@@ -28,7 +37,7 @@ class MainActivity : AppCompatActivity() {
             sharedPreferences.edit().putBoolean(KEY_WELCOME, true).apply()
         }
         else{
-            //trimit email-ul prin bundle catre edit fragment
+            //trimit email-ul prin bundle catre lists fragment
             val bundle = Bundle()
             bundle.putString(USER_EMAIL, userEmail)
             bundle.putString(USER_FULLNAME, userFullname)
@@ -40,5 +49,27 @@ class MainActivity : AppCompatActivity() {
                 .add(R.id.fragment_container, fragment, null)
                 .commit()
         }
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.lists -> replaceFragment(ListsFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+
+                else -> { }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val bundle = Bundle()
+        bundle.putString(USER_EMAIL, userEmail)
+        bundle.putString(USER_FULLNAME, userFullname)
+
+        fragment.arguments = bundle
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment, null)
+            .commit()
     }
 }
